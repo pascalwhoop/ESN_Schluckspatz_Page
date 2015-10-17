41ba12027377c5ac333303ee9dad249b8d641bca
Brokmeier, Pascal <Pascal.Brokmeier@opitz-consulting.com>
2015-10-17 15:02:32 +0200

    refactoring a bit

--- routes/formsubmit.js
+++ routes/formsubmit.js
@@ -3,28 +3,29 @@ var router = express.Router();
 var request = require('request');
 
 
-
-router.post("/", function(req,res,next){
+router.post("/", function (req, res, next) {
     var postData = {
         json: {
-            name : req.body.name,
-            text: "*Subject:* " + req.body.subject + "\n" + "*Message:* " + req.body.message + "\n\n *To Respond, use Email:* " + req.body.email
+            username: req.body.name,
+            text: getSlackMessage(req)
         }
     };
 
-    console.log(JSON.stringify(postData));
+    function getSlackMessage(req) {
+        return "*Subject:* " + req.body.subject + "\n" + "*Message:* " + req.body.message + "\n\n *To Respond, use Email:* " + req.body.email;
+    }
 
+    console.log(JSON.stringify(postData));
     request.post(
         "https://hooks.slack.com/services/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", postData,
-    function(err, res, body){
-        if (!err && res.statusCode == 200) {
-            console.log(body + " no foo")
-        }
-        else{
-            console.log(err + " foo ");
-            console.log(res.body + res.statusCode);
-        }
-    });
+        function (SErr, SRes, SBody) {
+            if (!SErr && SRes.statusCode == 200) {
+                res.sendStatus(200);
+            }
+            else {
+                res.sendStatus(SRes.statusCode);
+            }
+        });
 
 
 });
